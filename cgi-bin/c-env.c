8a52433b7370bfb50668e060c152e7fcd392cf5a
OctFog <xix063@ucsd.edu>
2025-08-18 17:48:41 -0700

    Init for HW2

---
+++ cgi-bin/c-env.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char **argv, char **envp)
+{
+  // print HTML header	
+  printf("Cache-Control: no-cache\n");
+  printf("Content-type: text/html\n\n");
+  printf("<html><head><title>Environment Variables</title></head> \
+	<body><h1 align=center>Environment Variables</h1> \
+  	<hr/>\n");
+  printf("<p>Hello, my name is <strong>Xiaogneg Xu</strong></p>");
+  
+
+  for (char **env = envp; *env != 0; env++)
+  {
+    char *thisEnv = *env;
+    printf("%s\n<br/>", thisEnv);
+  }
+
+  // print HTML footer
+  printf("</body></html>");
+  return 0;
+}
