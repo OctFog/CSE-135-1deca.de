a770367bcc5150485df88b309741f37fbc8ed25c
OctFog <xix063@ucsd.edu>
2025-08-19 01:05:32 -0700

    Update PHP echo files

--- cgi-bin/php-post-echo.php
+++ cgi-bin/php-post-echo.php
@@ -7,19 +7,23 @@ header("Content-Type: text/html");
 <head>
     <title>POST Request Echo</title>
     <style>
-         th, td { border: 1px solid black}
+        th, td { border: 1px solid black; }
     </style>
 </head>
 <body>
     <h1>POST Request Echo</h1>
-    <table>
-        <tr><th>Parameter</th><th>Value</th></tr>
-        <?php foreach ($_POST as $key => $value): ?>
-            <tr>
-                <td><?= htmlspecialchars($key) ?></td>
-                <td><?= htmlspecialchars($value) ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+    <?php if (!empty($_POST)): ?>
+        <table>
+            <tr><th>Parameter</th><th>Value</th></tr>
+            <?php foreach ($_POST as $key => $value): ?>
+                <tr>
+                    <td><?= htmlspecialchars($key) ?></td>
+                    <td><?= htmlspecialchars($value) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php else: ?>
+        <p style="text-align:center; font-style:italic;">No POST parameters were sent.</p>
+    <?php endif; ?>
 </body>
 </html>
